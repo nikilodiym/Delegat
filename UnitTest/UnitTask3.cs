0a15b88037168e460ce38ea7c9165f75d481bf28
Nikita Krenov <nikitabaram@gmail.com>
2024-12-27 20:03:38 +0100

    ### Summary of Most Important Changes The code changes primarily involve the addition of using directives, the introduction of new namespaces and classes, and the implementation of various methods to check numerical properties. Specifically, the `UnitTask3.cs` file now contains a unit test class with methods to check if a number is even or odd, while the `Task3.cs` file includes a class with methods to check if a number is even, odd, prime, or a Fibonacci number.
    ### List of Changes
    1. **Added using directives**: Included `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks` in both `UnitTask3.cs` and `Task3.cs` files. [UnitTask3.cs, Task3.cs]
    2. **Introduced new namespace and class in UnitTask3.cs**: Created a new namespace `UnitTest` and defined an internal class `UnitTask3` within it. [UnitTask3.cs]
    3. **Added private static field**: Added a private static integer field `number` in the `UnitTask3` class. [UnitTask3.cs]
    4. **Added `[TestClass]` attribute**: Marked the `UnitTask3` class with the `[TestClass]` attribute to indicate it contains unit tests. [UnitTask3.cs]
    5. **Added `[TestMethod]` attribute**: Marked the `IsEven` method in the `UnitTask3` class with the `[TestMethod]` attribute to indicate it is a test method. [UnitTask3.cs]
    6. **Implemented `IsEven` method in UnitTask3.cs**: Added a method to check if a number is even. [UnitTask3.cs]
    7. **Implemented `IsOdd` method in UnitTask3.cs**: Added a method to check if a number is odd. [UnitTask3.cs]
    8. **Introduced new namespace and class in Task3.cs**: Created a new namespace `delegats` and defined a public class `Task3` within it. [Task3.cs]
    9. **Implemented `IsEven` method in Task3.cs**: Added a method to check if a number is even. [Task3.cs]
    10. **Implemented `IsOdd` method in Task3.cs**: Added a method to check if a number is odd. [Task3.cs]
    11. **Implemented `IsPrime` method**: Added a method to check if a number is prime. [Task3.cs]
    12. **Implemented `IsFibonacci` method**: Added a method to check if a number is a Fibonacci number. [Task3.cs]

---
+++ UnitTest/UnitTask3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    internal class UnitTask3
+    {
+        private static int number;
+
+        [TestMethod]
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool IsOdd(int)
+        {
+            return number % 2 != 0;
+        }
+
+    }
+}
---
+++ delegats/Task3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegats
+{
+    public class Task3
+    {
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsFibonacci(int number)
+        {
+            int a = 0;
+            int b = 1;
+
+            while (b < number)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return b == number;
+        }
+    }
+}

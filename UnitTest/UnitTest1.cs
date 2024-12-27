ef978506f240e9a28ebcbe79afea9f5b1d614659
Nikita Krenov <nikitabaram@gmail.com>
2024-12-27 20:15:06 +0100

    ### Summary of Most Important Changes: The primary updates involve adding functionality to filter even, odd, prime, and Fibonacci numbers from an array in two different files: `UnitTest1.cs` and `Task1.cs`. In `UnitTest1.cs`, new methods and helper methods have been added and tested. In `Task1.cs`, similar functionality has been implemented using delegates and a generic method.
    ### List of Changes:
    1. **UnitTest1.cs (UnitTest namespace):**
       - Added methods for filtering numbers: `GetEvenNumbers`, `GetOddNumbers`, `GetPrimeNumbers`, and `GetFibonacciNumbers`. [UnitTest1.cs]
       - Added helper methods: `IsPrime` and `IsFibonacci` to determine if a number is prime or a Fibonacci number. [UnitTest1.cs]
       - Updated `TestMethod1` to test the new filtering methods using `CollectionAssert.AreEqual`. [UnitTest1.cs]
    
    2. **Task1.cs (delegats namespace):**
       - Added a delegate `NumberFilter` for filtering numbers. [Task1.cs]
       - Added a generic method `GetNumbers` that takes an array and a filter delegate to filter numbers. [Task1.cs]
       - Added specific filter methods: `IsEven`, `IsOdd`, `IsPrime`, and `IsFibonacci` to be used with the `GetNumbers` method. [Task1.cs]

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -6,6 +6,97 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> evenNumbers = GetEvenNumbers(array);
+            List<int> oddNumbers = GetOddNumbers(array);
+            List<int> primeNumbers = GetPrimeNumbers(array);
+            List<int> fibonacciNumbers = GetFibonacciNumbers(array);
+
+            CollectionAssert.AreEqual(new int[] { 2, 4, 6, 8, 10 }, evenNumbers);
+            CollectionAssert.AreEqual(new int[] { 1, 3, 5, 7, 9 }, oddNumbers);
+            CollectionAssert.AreEqual(new int[] { 2, 3, 5, 7 }, primeNumbers);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 5, 8 }, fibonacciNumbers);
+        }
+
+        public List<int> GetEvenNumbers(int[] array)
+        {
+            List<int> evenNumbers = new List<int>();
+            foreach (int num in array)
+            {
+                if (num % 2 == 0)
+                {
+                    evenNumbers.Add(num);
+                }
+            }
+            return evenNumbers;
+        }
+
+        public List<int> GetOddNumbers(int[] array)
+        {
+            List<int> oddNumbers = new List<int>();
+            foreach (int num in array)
+            {
+                if (num % 2 != 0)
+                {
+                    oddNumbers.Add(num);
+                }
+            }
+            return oddNumbers;
+        }
+
+        public List<int> GetPrimeNumbers(int[] array)
+        {
+            List<int> primeNumbers = new List<int>();
+            foreach (int num in array)
+            {
+                if (IsPrime(num))
+                {
+                    primeNumbers.Add(num);
+                }
+            }
+            return primeNumbers;
+        }
+
+        public List<int> GetFibonacciNumbers(int[] array)
+        {
+            List<int> fibonacciNumbers = new List<int>();
+            foreach (int num in array)
+            {
+                if (IsFibonacci(num))
+                {
+                    fibonacciNumbers.Add(num);
+                }
+            }
+            return fibonacciNumbers;
+        }
+
+        public bool IsPrime(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsFibonacci(int num)
+        {
+            int a = 0;
+            int b = 1;
+            while (b < num)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return b == num;
         }
     }
 }
--- delegats/Task1.cs
+++ delegats/Task1.cs
@@ -2,6 +2,84 @@
 {
     public class Task1
     {
+        public delegate bool NumberFilter(int number);
 
+        public static List<int> GetEvenNumbers(int[] array)
+        {
+            return GetNumbers(array, IsEven);
+        }
+
+        public static List<int> GetOddNumbers(int[] array)
+        {
+            return GetNumbers(array, IsOdd);
+        }
+
+        public static List<int> GetPrimeNumbers(int[] array)
+        {
+            return GetNumbers(array, IsPrime);
+        }
+
+        public static List<int> GetFibonacciNumbers(int[] array)
+        {
+            return GetNumbers(array, IsFibonacci);
+        }
+
+        private static List<int> GetNumbers(int[] array, NumberFilter filter)
+        {
+            List<int> numbers = new List<int>();
+
+            foreach (int number in array)
+            {
+                if (filter(number))
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            return numbers;
+        }
+
+        private static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        private static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsFibonacci(int number)
+        {
+            int a = 0;
+            int b = 1;
+
+            while (b < number)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return b == number;
+        }
     }
 }

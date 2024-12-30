42965648fefa51716eaec8c96772df25f84fa7bd
Nikita Krenov <nikitabaram@gmail.com>
2024-12-30 11:50:22 +0100

    ### Summary of Most Important Changes: 1. **Introduction of Unit Tests**: A new file `UnitTest2.cs` was added, which includes unit tests for various methods in the `Task2` class. 2. **New Functionalities in Task2 Class**: A new file `Task2.cs` was added, which includes methods for displaying the current time, date, day of the week, and calculating the areas of triangles and rectangles.
    ### List of Changes:
    1. **Added UnitTest2.cs**:
       - **Namespace**: `UnitTest`
       - **Class**: `UnitTest2` (internal)
       - **Unit Tests**:
         - `TestDisplayCurrentTime()`: Tests the `DisplayCurrentTime` method. [UnitTest2.cs]
         - `TestDisplayCurrentDate()`: Tests the `DisplayCurrentDate` method. [UnitTest2.cs]
         - `TestDisplayCurrentDayOfWeek()`: Tests the `DisplayCurrentDayOfWeek` method. [UnitTest2.cs]
         - `TestCalculateTriangleArea()`: Tests the `CalculateTriangleArea` method with a base length of 10 and height of 5, expecting the result to be 25. [UnitTest2.cs]
         - `TestCalculateRectangleArea()`: Tests the `CalculateRectangleArea` method with a length of 10 and width of 5, expecting the result to be 50. [UnitTest2.cs]
    
    2. **Added Task2.cs**:
       - **Namespace**: `delegats`
       - **Class**: `Task2` (public)
       - **Methods**:
         - `DisplayCurrentTime()`: Displays the current time in "HH:mm:ss" format. [Task2.cs]
         - `DisplayCurrentDate()`: Displays the current date in "MM/dd/yyyy" format. [Task2.cs]
         - `DisplayCurrentDayOfWeek()`: Displays the current day of the week in "dddd" format. [Task2.cs]
         - `CalculateTriangleArea(double baseLength, double height)`: Calculates the area of a triangle given the base length and height. [Task2.cs]
         - `CalculateRectangleArea(double length, double width)`: Calculates the area of a rectangle given the length and width. [Task2.cs]

---
+++ UnitTest/UnitTest2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestDisplayCurrentTime()
+        {
+            Task2.DisplayCurrentTime();
+        }
+
+        [TestMethod]
+        public void TestDisplayCurrentDate()
+        {
+            Task2.DisplayCurrentDate();
+        }
+
+        [TestMethod]
+        public void TestDisplayCurrentDayOfWeek()
+        {
+            Task2.DisplayCurrentDayOfWeek();
+        }
+
+        [TestMethod]
+        public void TestCalculateTriangleArea()
+        {
+            double baseLength = 10;
+            double height = 5;
+            double expected = 25;
+            double actual = Task2.CalculateTriangleArea(baseLength, height);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestCalculateRectangleArea()
+        {
+            double length = 10;
+            double width = 5;
+            double expected = 50;
+            double actual = Task2.CalculateRectangleArea(length, width);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ delegats/Task2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegats
+{
+    public class Task2
+    {
+        public static void DisplayCurrentTime()
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("Current Time: " + currentTime.ToString("HH:mm:ss"));
+        }
+
+        public static void DisplayCurrentDate()
+        {
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine("Current Date: " + currentDate.ToString("MM/dd/yyyy"));
+        }
+
+        public static void DisplayCurrentDayOfWeek()
+        {
+            DateTime currentDay = DateTime.Now;
+            Console.WriteLine("Current Day of Week: " + currentDay.ToString("dddd"));
+        }
+
+        public static double CalculateTriangleArea(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+
+        public static double CalculateRectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+    }
+}
